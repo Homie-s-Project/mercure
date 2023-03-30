717bd1ff27c00499c0313505392398d64943be09
WilliamDevv <wpasquier61@gmail.com>
2023-03-30 08:44:25 +0200

    update homepage

--- mercure-web/src/app/components/item-card/item-card.component.ts
+++ mercure-web/src/app/components/item-card/item-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faTag } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-item-card',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./item-card.component.scss']
 })
 export class ItemCardComponent implements OnInit {
+  faTag = faTag;
 
   constructor() { }
 
