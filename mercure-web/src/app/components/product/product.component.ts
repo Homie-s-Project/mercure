106c91b71a906374c3615a7936168748bd6aa627
WilliamDevv <wpasquier61@gmail.com>
2023-04-03 22:34:08 +0200

    continue page

--- mercure-web/src/app/components/product/product.component.ts
+++ mercure-web/src/app/components/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faCartPlus } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-product',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./product.component.scss']
 })
 export class ProductComponent {
-
+  faCartPlus = faCartPlus;
 }
