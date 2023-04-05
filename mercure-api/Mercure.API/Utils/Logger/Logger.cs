f8eb065722b97559633cafb304a9c996003523b5
Romain Antunes <romainantunes2003@hotmail.com>
2023-04-05 18:30:33 +0200

    Add compatibility with windows log.

--- mercure-api/Mercure.API/Utils/Logger/Logger.cs
+++ mercure-api/Mercure.API/Utils/Logger/Logger.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
 
 namespace Mercure.API.Utils.Logger;
 
@@ -10,39 +12,122 @@ public class Logger
         switch (logLevel)
         {
             case LogLevel.Trace:
-                logText += $"\u001b[37m[Trace]\u001b[0m ";
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    logText += "[Trace]";
+                }
+                else
+                {
+                    logText += $"\u001b[37m[Trace]\u001b[0m ";
+                }
                 break;
             case LogLevel.Debug:
-                logText += $"\u001b[34m[Debug]\u001b[0m ";
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    logText += "[Debug]";
+                }
+                else
+                {
+                    logText += $"\u001b[34m[Debug]\u001b[0m ";
+                }
                 break;
             case LogLevel.Info:
-                logText += $"\u001b[32m[Info]\u001b[0m ";
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    logText += "[Info]";
+                }
+                else
+                {
+                    logText += $"\u001b[32m[Info]\u001b[0m ";
+                }
                 break;
             case LogLevel.Warn:
-                logText += $"\u001b[33m[Warn]\u001b[0m ";
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    logText += "[Warn]";
+                }
+                else
+                {
+                    logText += $"\u001b[33m[Warn]\u001b[0m ";
+                }
+
                 break;
             case LogLevel.Error:
-                logText += $"\u001b[31m[Error]\u001b[0m ";
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    logText += "[Error]";
+                }
+                else
+                {
+                    logText += $"\u001b[31m[Error]\u001b[0m ";
+                }
                 break;
             case LogLevel.Critical:
-                logText += $"\u001b[35m[Critical]\u001b[0m ";
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    logText += "[Critica]";
+                }
+                else
+                {
+                    logText += $"\u001b[35m[Critical]\u001b[0m ";
+                }
                 break;
             case LogLevel.None:
                 break;
             default:
                 throw new ArgumentException("Invalid log level");
         }
-        
+
         logText += $"[{logTarget}] {message}";
-        Console.WriteLine(logText);
+
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            if (logLevel == LogLevel.Error || logLevel == LogLevel.Critical)
+            {
+                WriteColorForWindows(logText, ConsoleColor.Red);
+            } else if (logLevel == LogLevel.Warn)
+            {
+                WriteColorForWindows(logText, ConsoleColor.Yellow);
+            }
+            else
+            {
+                WriteColorForWindows(logText, ConsoleColor.White);
+            }
+        }
+        else
+        {
+            Console.WriteLine(logText);
+
+        }
     }
 
     public static void LogInfo(string message) => Log(LogLevel.Info, LogTarget.File, message);
     public static void LogInfo(LogTarget logTarget, string message) => Log(LogLevel.Info, logTarget, message);
-    
+
     public static void LogWarn(string message) => Log(LogLevel.Info, LogTarget.File, message);
     public static void LogWarn(LogTarget logTarget, string message) => Log(LogLevel.Info, logTarget, message);
-    
+
     public static void LogError(string message) => Log(LogLevel.Info, LogTarget.File, message);
     public static void LogError(LogTarget logTarget, string message) => Log(LogLevel.Info, logTarget, message);
+    
+    private static void WriteColorForWindows(string message, ConsoleColor color)
+    {
+        var pieces = Regex.Split(message, @"(\[[^\]]*\])");
+
+        for(int i=0;i<pieces.Length;i++)
+        {
+            string piece = pieces[i];
+        
+            if (piece.StartsWith("[") && piece.EndsWith("]"))
+            {
+                Console.ForegroundColor = color;
+                piece = piece.Substring(1,piece.Length-2);          
+            }
+        
+            Console.Write(piece);
+            Console.ResetColor();
+        }
+    
+        Console.WriteLine();
+    }
 }
