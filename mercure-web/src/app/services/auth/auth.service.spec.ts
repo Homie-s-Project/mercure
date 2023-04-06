7ccc3990cd07b27795d7fe852f16490d431a44b5
Alexandre Botta <03alexbotta@gmail.com>
2023-04-06 12:30:09 +0200

    Correction test

--- mercure-web/src/app/services/auth/auth.service.spec.ts
+++ mercure-web/src/app/services/auth/auth.service.spec.ts
@@ -1,4 +1,8 @@
 import { TestBed } from '@angular/core/testing';
+import {HttpClient, HttpHeaders, HttpResponse} from '@angular/common/http';
+import { CookieService } from 'ngx-cookie-service';
+import { Observable } from 'rxjs';
+import { UserModel } from 'src/app/models/UserModel';
 
 import { AuthService } from './auth.service';
 
@@ -6,7 +10,11 @@ describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      declarations: [ AuthService ],
+      providers: [HttpClient, HttpHeaders, HttpResponse, CookieService, Observable, UserModel]
+    })
+    .compileComponents();
     service = TestBed.inject(AuthService);
   });
 
