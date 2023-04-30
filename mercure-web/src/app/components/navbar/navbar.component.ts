0a65e14e38367e6110c73b3c1e643b8d454e5189
Chriss052 <andrade.christopher@hotmail.com>
2023-04-30 20:03:32 +0200

    Changement d'icone de l'aperçu du panier et ajustement du texte des produit

--- mercure-web/src/app/components/navbar/navbar.component.ts
+++ mercure-web/src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,6 @@
 import {Component, EventEmitter, Output} from '@angular/core';
 import { faCartShopping, faUser, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
-
+import { faXmark} from "@fortawesome/free-solid-svg-icons";
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -9,13 +9,15 @@ import { faCartShopping, faUser, faMagnifyingGlass } from '@fortawesome/free-sol
 export class NavbarComponent {
   faCartShopping = faCartShopping
   faUser = faUser;
+  faXmark = faXmark;
+  actualIcone = this.faXmark;
   faMagnifyingGlass = faMagnifyingGlass;
   @Output() toggleHide = new EventEmitter<boolean>();
   hideCart: boolean = true
-
   constructor() { }
 
   toggleShowHide() {
+    this.actualIcone = this.actualIcone === this.faXmark ? this.faCartShopping : this.faXmark;
     this.hideCart = !this.hideCart;
     this.toggleHide.emit(this.hideCart);
   }
