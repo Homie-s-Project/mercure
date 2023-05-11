3bf5c5b1ee79c1b42ce3868a2f1b5b6174f578ed
Romain Antunes <romainantunes2003@hotmail.com>
2023-05-11 08:30:26 +0200

    fix: unit test

--- mercure-web/src/app/guard/authentified/authentified.guard.spec.ts
+++ mercure-web/src/app/guard/authentified/authentified.guard.spec.ts
@@ -1,12 +1,18 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { AuthentifiedGuard } from './authentified.guard';
+import {AuthentifiedGuard} from './authentified.guard';
+import {AuthService} from "../../services/auth/auth.service";
+import {RouterTestingModule} from "@angular/router/testing";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
 
 describe('AuthentifiedGuard', () => {
   let guard: AuthentifiedGuard;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [AuthService],
+      imports: [ RouterTestingModule, HttpClientTestingModule ]
+    });
     guard = TestBed.inject(AuthentifiedGuard);
   });
 
--- mercure-web/src/app/guard/unauthentified/unauthentified.guard.spec.ts
+++ mercure-web/src/app/guard/unauthentified/unauthentified.guard.spec.ts
@@ -1,12 +1,18 @@
 import { TestBed } from '@angular/core/testing';
 
 import { UnauthentifiedGuard } from './unauthentified.guard';
+import {AuthService} from "../../services/auth/auth.service";
+import {RouterTestingModule} from "@angular/router/testing";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
 
 describe('UnauthentifiedGuard', () => {
   let guard: UnauthentifiedGuard;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [AuthService],
+      imports: [ RouterTestingModule, HttpClientTestingModule ]
+    });
     guard = TestBed.inject(UnauthentifiedGuard);
   });
 
